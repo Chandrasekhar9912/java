65aece6bce78c36245f1f29090c2e25279162f51
Chandrasekhar9912 <111849510+Chandrasekhar9912@users.noreply.github.com>
2022-08-23 12:52:50 +0530

    Add files via upload

---
+++ src/Demoarray.java
@@ -0,0 +1,20 @@
+
+public class Demoarray {
+
+	public static void main(String[] args) {
+		String[] mycars = new String[3];
+		//to assign values
+		
+		mycars[0]="Bmw";
+		mycars[1]="Audi";
+		mycars[2]="Benz";
+System.out.println("number of values in array are:"+mycars.length);
+
+// to read 2nd value
+
+System.out.println(mycars[1]);
+
+
+	}
+
+}
